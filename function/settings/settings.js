15f9cd7010f845e7653567995ed1ed84168298e6
mehsjxbkaab <mehsjxbkaab@gmail.com>
2026-03-12 12:50:33 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,8 +14,8 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '212675617959',
+ pairingNumber : '212675617959',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
